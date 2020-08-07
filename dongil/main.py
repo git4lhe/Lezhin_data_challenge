506bf34227e872be6f62fd08db00d1fe67547c1e
ryudongil <jules010@naver.com>
2020-08-07 17:42:16 +0900

    [CREATE] Crete Modeler Module
    - use mdl_inst_list in Modeler module for storing model instance
    - make cross_validation function which scores roc_auc and folds with stratified strategy
    - in algorithm, logistic regression, random forest, xgboost are used

--- dongil/main.py
+++ dongil/main.py
@@ -1,22 +1,27 @@
 from DataCollector.DataCollector import DataCollector
 from DataCleanser.DataCleanser import DataCleanser
 from DataCleanser.Scaler import Scaler
+from Modeler.Modeler import Modeler
 
 
 if __name__ == '__main__':
     dataCollector = DataCollector(data_folder_path='../data')
-    df = dataCollector.read_csv_to_df('lezhin_dataset_v2_test.tsv')
+    df = dataCollector.read_csv_to_df('lezhin_dataset_v2_training.tsv')
 
     y_col = 1
     dataCleanser = DataCleanser(y_col=y_col)
     df = dataCleanser.remove_na_col(df, threshold=0.1)
     df = dataCleanser.remove_str_col(df)
-    df = dataCleanser.remove_cate_col(df, threshold=2)
+    df = dataCleanser.remove_cate_col(df, threshold=1)
 
     x_cols = [x for x in df.columns if x != y_col]
     scaler = Scaler(y_col=y_col, x_cols=x_cols, scaler_method='MinMax')
     scaler.fit_scaler(df)
     df = scaler.transform_df(df)
 
-    print(df)
+    mdl_name_list = ['logistic', 'random_forest', 'xgboost']
+    modeler = Modeler(y_col=y_col, x_cols=x_cols, mdl_name_list=mdl_name_list)
+    result = modeler.cross_validation(df)
+
+    print(result)
 
