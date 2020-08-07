75708ecb4f2c6e259fb22c32e323b1ee6e7e964a
ryudongil <jules010@naver.com>
2020-08-07 16:38:06 +0900

    [CREATE] create DataCleanser Module
    - create function that removes NA cols
    - create function that removes string cols
    - create function that removes categorical cols

--- dongil/main.py
+++ dongil/main.py
@@ -1,7 +1,18 @@
 from DataCollector.DataCollector import DataCollector
+from DataCleanser.DataCleanser import DataCleanser
 
 
 if __name__ == '__main__':
     dataCollector = DataCollector(data_folder_path='../data')
     df = dataCollector.read_csv_to_df('lezhin_dataset_v2_test.tsv')
+
+    y_col = 1
+    dataCleanser = DataCleanser(y_col=y_col)
+    df = dataCleanser.remove_na_col(df, threshold=0.1)
+    df = dataCleanser.remove_str_col(df)
+    df = dataCleanser.remove_cate_col(df, threshold=2)
+
+    print(dataCleanser.na_col_list)
+    print(dataCleanser.str_col_list)
+    print(dataCleanser.cate_col_list)
     print(df.columns)
