83ac35c37d227653c6deaacdaa6e2bb21579fd6a
ryudongil <jules010@naver.com>
2020-08-14 01:42:28 +0900

    [CREATE] Create FeatureController Module
    - impute NA columns which have explanation ability (fill_value would be 2.0 that is outlier in column)
    - make purchase ratio column using work hash code column

--- dongil/FeatureController/FeatureController.py
+++ dongil/FeatureController/FeatureController.py
@@ -1,6 +1,29 @@
-
+import pandas as pd
+from sklearn.impute import SimpleImputer
 
 class FeatureController:
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, y_col):
+        self.y_col = y_col
+
+        self.fill_value = 2.0
+        self.count_threshold = 100
+        self.right_suffix = '_mean'
+
+    def custom_imputation(self, df, impute_col_list):
+        imp = SimpleImputer(strategy='constant', fill_value=self.fill_value)
+        df[impute_col_list] = imp.fit_transform(df[impute_col_list].to_numpy())
+
+    def make_purchase_ratio_column(self, df, x_col):
+        y_mean = df[self.y_col].mean()
+
+        mean_df = df.groupby(x_col)[self.y_col].mean()
+        count_df = df.groupby(x_col)[self.y_col].count()
+        over_threshold_idx = count_df[count_df > self.count_threshold].index
+
+        merge_df = pd.merge(df, mean_df[over_threshold_idx],
+                            how='left', left_on=x_col, right_index=True,
+                            suffixes=('', self.right_suffix))
+        merge_df[''.join([str(self.y_col), self.right_suffix])].fillna(y_mean, inplace=True)
+
+        return merge_df
--- dongil/main.py
+++ dongil/main.py
@@ -3,6 +3,7 @@ from DataCleanser.DataCleanser import DataCleanser
 from DataCleanser.Scaler import Scaler
 from Modeler.Modeler import Modeler
 from DataVisualizer.DataVisualizer import DataVisualizer
+from FeatureController.FeatureController import FeatureController
 
 
 if __name__ == '__main__':
@@ -11,25 +12,30 @@ if __name__ == '__main__':
 
     y_col = 1
 
-    dataVisualizer = DataVisualizer(y_col=y_col, save_path='../img')
-    for x_col in range(152, 168):
-        dataVisualizer.na_col_plot(_df=df, x_col=x_col)
-    for x_col in (7, 8, 10):
-        dataVisualizer.str_col_plot(_df=df, x_col=x_col)
+    # dataVisualizer = DataVisualizer(y_col=y_col, save_path='../img')
+    # for x_col in range(152, 168):
+    #     dataVisualizer.na_col_plot(_df=df, x_col=x_col)
+    # for x_col in (7, 8, 10):
+    #     dataVisualizer.str_col_plot(_df=df, x_col=x_col)
 
-    # dataCleanser = DataCleanser(y_col=y_col)
-    # df = dataCleanser.remove_na_col(df, threshold=0.1)
-    # df = dataCleanser.remove_str_col(df)
-    # df = dataCleanser.remove_cate_col(df, threshold=1)
-    #
-    # x_cols = [x for x in df.columns if x != y_col]
-    # scaler = Scaler(y_col=y_col, x_cols=x_cols, scaler_method='MinMax')
-    # scaler.fit_scaler(df)
-    # df = scaler.transform_df(df)
-    #
-    # mdl_name_list = ['logistic', 'random_forest', 'xgboost']
-    # modeler = Modeler(y_col=y_col, x_cols=x_cols, mdl_name_list=mdl_name_list)
-    # result = modeler.cross_validation(df)
-    #
-    # print(result)
+    impute_col_list = [156, 159, 164, 165]
+    featureController = FeatureController(y_col=y_col)
+    featureController.custom_imputation(df=df, impute_col_list=impute_col_list)
+    df = featureController.make_purchase_ratio_column(df=df, x_col=7)
+
+    dataCleanser = DataCleanser(y_col=y_col)
+    df = dataCleanser.remove_na_col(df, threshold=0.1)
+    df = dataCleanser.remove_str_col(df)
+    df = dataCleanser.remove_cate_col(df, threshold=1)
+
+    x_cols = [x for x in df.columns if x != y_col]
+    scaler = Scaler(y_col=y_col, x_cols=x_cols, scaler_method='MinMax')
+    scaler.fit_scaler(df)
+    df = scaler.transform_df(df)
+
+    mdl_name_list = ['logistic', 'random_forest', 'xgboost']
+    modeler = Modeler(y_col=y_col, x_cols=x_cols, mdl_name_list=mdl_name_list)
+    result = modeler.cross_validation(df)
+
+    print(result)
 
