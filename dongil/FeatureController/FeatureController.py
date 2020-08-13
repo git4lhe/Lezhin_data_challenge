a0a12b867d119e880ba1c59dc039c58a667963f6
ryudongil <jules010@naver.com>
2020-08-14 01:51:33 +0900

    [BUG FIX] rename Series to avoid column name overlap
    - when overlap, y_col is renamed to string '1'
    - to avoid this, rename Series name

--- dongil/FeatureController/FeatureController.py
+++ dongil/FeatureController/FeatureController.py
@@ -8,7 +8,7 @@ class FeatureController:
 
         self.fill_value = 2.0
         self.count_threshold = 100
-        self.right_suffix = '_mean'
+        self.new_col_name = 'purchase_ratio'
 
     def custom_imputation(self, df, impute_col_list):
         imp = SimpleImputer(strategy='constant', fill_value=self.fill_value)
@@ -17,13 +17,12 @@ class FeatureController:
     def make_purchase_ratio_column(self, df, x_col):
         y_mean = df[self.y_col].mean()
 
-        mean_df = df.groupby(x_col)[self.y_col].mean()
+        mean_df = df.groupby(x_col)[self.y_col].mean().rename(self.new_col_name)
         count_df = df.groupby(x_col)[self.y_col].count()
         over_threshold_idx = count_df[count_df > self.count_threshold].index
 
         merge_df = pd.merge(df, mean_df[over_threshold_idx],
-                            how='left', left_on=x_col, right_index=True,
-                            suffixes=('', self.right_suffix))
-        merge_df[''.join([str(self.y_col), self.right_suffix])].fillna(y_mean, inplace=True)
+                            how='left', left_on=x_col, right_index=True)
+        merge_df[self.new_col_name].fillna(y_mean, inplace=True)
 
         return merge_df
