69d9627abbccebbd0d00e1973c573abe7a67975c
ryudongil <jules010@naver.com>
2020-08-14 01:29:13 +0900

    [CREATE] Create DataVisualizer Module
    - make na_col_plot function for choosing column that has NA but can explain Y
    - make str_col_plot function for visualizing column which type is String
    - update .gitignore to ignore img files

--- dongil/DataVisualizer/DataVisualizer.py
+++ dongil/DataVisualizer/DataVisualizer.py
@@ -1,6 +1,53 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 class DataVisualizer:
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, y_col, save_path):
+        self.y_col = y_col
+        self.save_path = save_path
+
+        self.fill_na_val = 2.0
+        os.makedirs(save_path, exist_ok=True)
+
+    def na_col_plot(self, _df, x_col):
+        df = _df.copy()
+
+        bins = [0.1 * x for x in range(12)]
+        labels = [0.1 * x for x in range(10)] + [self.fill_na_val]
+
+        df['binned'] = pd.cut(df[x_col], bins=bins, labels=labels)
+        df['binned'].fillna(value=self.fill_na_val, inplace=True)
+
+        mean_df = df.groupby('binned')[self.y_col].mean()
+        count_df = df.groupby('binned')[self.y_col].count()
+        names = [str(round(x, 2)) for x in mean_df.index]
+        mean_values = [round(x, 3) for x in mean_df.values]
+        count_ratio_values = [round(x/len(df), 3) for x in count_df.values]
+
+        title = f'Column {x_col} Binning'
+        plt.figure(figsize=(18, 6))
+        plt.subplot(131)
+        plt.bar(names, mean_values)
+        plt.subplot(133)
+        plt.bar(names, count_ratio_values)
+        plt.suptitle(title)
+        plt.savefig(os.path.join(self.save_path, f"{title.replace(' ', '_')}.png"), dpi=1000)
+
+    def str_col_plot(self, _df, x_col):
+        df = _df.copy()
+
+        mean_df = df.groupby(x_col)[self.y_col].mean()
+        count_df = df.groupby(x_col)[self.y_col].count()
+        mean_values = [round(x, 3) for x in mean_df.values]
+
+        title = f'Column {x_col} Grouping'
+        plt.figure(figsize=(18, 6))
+        plt.subplot(121)
+        plt.plot(sorted(mean_values))
+        plt.subplot(133)
+        plt.plot(sorted(count_df.values))
+        plt.suptitle(title)
+        plt.savefig(os.path.join(self.save_path, f"{title.replace(' ', '_')}.png"), dpi=1000)
--- dongil/main.py
+++ dongil/main.py
@@ -2,6 +2,7 @@ from DataCollector.DataCollector import DataCollector
 from DataCleanser.DataCleanser import DataCleanser
 from DataCleanser.Scaler import Scaler
 from Modeler.Modeler import Modeler
+from DataVisualizer.DataVisualizer import DataVisualizer
 
 
 if __name__ == '__main__':
@@ -9,19 +10,26 @@ if __name__ == '__main__':
     df = dataCollector.read_csv_to_df('lezhin_dataset_v2_training.tsv')
 
     y_col = 1
-    dataCleanser = DataCleanser(y_col=y_col)
-    df = dataCleanser.remove_na_col(df, threshold=0.1)
-    df = dataCleanser.remove_str_col(df)
-    df = dataCleanser.remove_cate_col(df, threshold=1)
 
-    x_cols = [x for x in df.columns if x != y_col]
-    scaler = Scaler(y_col=y_col, x_cols=x_cols, scaler_method='MinMax')
-    scaler.fit_scaler(df)
-    df = scaler.transform_df(df)
+    dataVisualizer = DataVisualizer(y_col=y_col, save_path='../img')
+    for x_col in range(152, 168):
+        dataVisualizer.na_col_plot(_df=df, x_col=x_col)
+    for x_col in (7, 8, 10):
+        dataVisualizer.str_col_plot(_df=df, x_col=x_col)
 
-    mdl_name_list = ['logistic', 'random_forest', 'xgboost']
-    modeler = Modeler(y_col=y_col, x_cols=x_cols, mdl_name_list=mdl_name_list)
-    result = modeler.cross_validation(df)
-
-    print(result)
+    # dataCleanser = DataCleanser(y_col=y_col)
+    # df = dataCleanser.remove_na_col(df, threshold=0.1)
+    # df = dataCleanser.remove_str_col(df)
+    # df = dataCleanser.remove_cate_col(df, threshold=1)
+    #
+    # x_cols = [x for x in df.columns if x != y_col]
+    # scaler = Scaler(y_col=y_col, x_cols=x_cols, scaler_method='MinMax')
+    # scaler.fit_scaler(df)
+    # df = scaler.transform_df(df)
+    #
+    # mdl_name_list = ['logistic', 'random_forest', 'xgboost']
+    # modeler = Modeler(y_col=y_col, x_cols=x_cols, mdl_name_list=mdl_name_list)
+    # result = modeler.cross_validation(df)
+    #
+    # print(result)
 
