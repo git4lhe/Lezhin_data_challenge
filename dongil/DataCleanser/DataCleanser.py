57ebf34fdebbf8f0a68e8c3a551f9b60ccfcbb5a
ryudongil <jules010@naver.com>
2020-08-20 02:12:13 +0900

    [UPDATE] update DataCleanser Module for Pipeline
    - This module performs origin DataCleanser functions
    - To use Pipeline, we have to make class which has fit, fit_transform, transform, get_params function
    - get_params function is equal to get_params function in sklearn SimpleImputer class

--- dongil/DataCleanser/DataCleanser.py
+++ dongil/DataCleanser/DataCleanser.py
@@ -1,45 +1,75 @@
+import warnings
 import pandas as pd
+from sklearn.base import BaseEstimator
 
 
-class DataCleanser:
+class DataCleanser(BaseEstimator):
 
-    def __init__(self, y_col):
-        self.y_col = y_col
+    def __init__(self, na_threshold=0.1, cate_threshold=1):
+        self.na_threshold = na_threshold
+        self.cate_threshold = cate_threshold
 
-        # for debugging
         self.na_col_list = list()
         self.str_col_list = list()
         self.cate_col_list = list()
+        self.drop_col_list = list()
 
-    def remove_na_col(self, df, threshold):
-        self.na_col_list = df.columns[df.isna().sum() >= threshold * len(df)].tolist()
+    def _make_na_col_list(self, df):
+        self.na_col_list = df.columns[df.isna().sum() >= self.na_threshold * len(df)].tolist()
 
-        not_na_col_list = [x for x in df.columns if x not in self.na_col_list]
-        not_na_col_list.append(self.y_col)
-        not_na_col_list_with_y = list(set(not_na_col_list))
-
-        return df[not_na_col_list_with_y]
-
-    def remove_str_col(self, df):
-        not_str_col_list = list()
+    def _make_str_col_list(self, df):
         for col in df.columns:
             try:
                 df[col] = pd.to_numeric(df[col])
-                not_str_col_list.append(col)
             except ValueError:
                 self.str_col_list.append(col)
 
-        not_str_col_list.append(self.y_col)
-        not_str_col_list_with_y = list(set(not_str_col_list))
+    def _make_cate_col_list(self, df):
+        self.cate_col_list = df.columns[df.nunique() <= self.cate_threshold].tolist()
 
-        return df[not_str_col_list_with_y]
+    def fit(self, X, y=None):
+        df = pd.DataFrame(X)
+        self._make_na_col_list(df)
+        self._make_str_col_list(df)
+        self._make_cate_col_list(df)
 
-    def remove_cate_col(self, df, threshold):
-        self.cate_col_list = df.columns[df.nunique() <= threshold].tolist()
+        self.drop_col_list = list(set(self.na_col_list + self.str_col_list + self.cate_col_list))
 
-        not_cate_col_list = [x for x in df.columns if x not in self.cate_col_list]
-        not_cate_col_list.append(self.y_col)
-        not_cate_col_list_with_y = list(set(not_cate_col_list))
+    def transform(self, X):
+        df = pd.DataFrame(X)
+        remain_cols = [x for x in df.columns if x not in self.drop_col_list]
+        return df[remain_cols].to_numpy()
 
-        return df[not_cate_col_list_with_y]
+    def fit_transform(self, X, y=None):
+        self.fit(X=X, y=y)
+        return self.transform(X)
 
+    def get_params(self, deep=True):
+        """
+        Get parameters for this estimator.
+        Parameters
+        ----------
+        deep : bool, default=True
+            If True, will return the parameters for this estimator and
+            contained subobjects that are estimators.
+        Returns
+        -------
+        params : mapping of string to any
+            Parameter names mapped to their values.
+        """
+        out = dict()
+        for key in self._get_param_names():
+            try:
+                value = getattr(self, key)
+            except AttributeError:
+                warnings.warn('From version 0.24, get_params will raise an '
+                              'AttributeError if a parameter cannot be '
+                              'retrieved as an instance attribute. Previously '
+                              'it would return None.',
+                              FutureWarning)
+                value = None
+            if deep and hasattr(value, 'get_params'):
+                deep_items = value.get_params().items()
+                out.update((key + '__' + k, val) for k, val in deep_items)
+            out[key] = value
+        return out
