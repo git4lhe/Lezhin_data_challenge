9cafe908aeb7b972257476474ff68ca530c73a7b
ryudongil <jules010@naver.com>
2020-08-28 19:45:49 +0900

    [UPDATE] update main.py with grid search
    - make column_trans_list for making param_grid
    - make result file with cv_results_

--- dongil/main.py
+++ dongil/main.py
@@ -1,3 +1,7 @@
+import os
+import itertools
+import pandas as pd
+
 from DataCollector.DataCollector import DataCollector
 from DataCleanser.DataCleanser import DataCleanser
 from FeatureController.PurchaseRatioMaker import PurchaseRatioMaker
@@ -7,7 +11,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score, StratifiedKFold
+from sklearn.model_selection import cross_val_score, StratifiedKFold, GridSearchCV
 
 
 if __name__ == '__main__':
@@ -18,35 +22,57 @@ if __name__ == '__main__':
     y_col = '1'
     x_col_list = [x for x in df.columns if x != y_col]
 
-    impute_col_list = ['156', '159', '164', '165']
+    impute_col_list = [[], ['156', '159', '164', '165'], ['153', '154', '156', '159', '164', '165']]
     fill_value = 2.0
 
-    purchase_ratio_col_list = ['7', '8']
-    count_threshold = 100
+    purchase_col_list = [['7'], ['7', '8']]
+    thre_list = [30, 100]
     new_col_prefix = 'purchase_ratio'
 
-    column_trans = ColumnTransformer(
-        [('simple_impute', SimpleImputer(strategy='constant', fill_value=fill_value), impute_col_list),
-         ('purchase_ratio', PurchaseRatioMaker(y_col=y_col,
-                                               count_threshold=count_threshold,
-                                               new_col_prefix=new_col_prefix), purchase_ratio_col_list)],
-        remainder='passthrough')
+    column_trans_list = list()
+    for impute_cols, pr_cols, thre in itertools.product(impute_col_list, purchase_col_list, thre_list):
+        column_trans = ColumnTransformer(
+            [('simple_impute', SimpleImputer(strategy='constant', fill_value=fill_value), impute_cols),
+             ('purchase_ratio', PurchaseRatioMaker(y_col=y_col,
+                                                   count_threshold=thre,
+                                                   new_col_prefix=new_col_prefix), pr_cols)],
+            remainder='passthrough')
+        column_trans_list.append(column_trans)
 
     na_threshold = 0.1
     cate_threshold = 1
     dataCleanser = DataCleanser(na_threshold=na_threshold, cate_threshold=cate_threshold)
 
     pipe = Pipeline(
-        steps=[('column_trans', column_trans),
+        steps=[('column_trans', column_trans_list[0]),
                ('data_cleanser', dataCleanser),
                ('scaler', StandardScaler()),
-               ('random_foest', RandomForestClassifier(n_estimators=100, n_jobs=-1, class_weight='balanced'))],
+               ('random_forest', RandomForestClassifier(n_estimators=100, n_jobs=-1, class_weight='balanced'))],
         verbose=True
     )
 
+    param_grid = dict(column_trans=column_trans_list,
+                      scaler=[StandardScaler(), MinMaxScaler(), RobustScaler()],
+                      random_forest__n_estimators=[100, 300, 500],
+                      random_forest__max_depth=[5, 15])
+
     cv = StratifiedKFold(n_splits=5)
-    scores = cross_val_score(pipe, df[x_col_list], df[y_col], cv=cv, scoring='roc_auc')
-    print(scores.mean())
+    opt = GridSearchCV(pipe, param_grid, n_jobs=2, cv=cv, scoring='roc_auc')
+    opt.fit(df[x_col_list], df[y_col])
+
+    print('\nBest parameter (CV score = %0.3f):' % opt.best_score_)
+
+    # show results
+    result_path = '.'
+    csv_path = os.path.join(result_path, 'train')
+    os.makedirs(csv_path, exist_ok=True)
+
+    df = pd.DataFrame(opt.cv_results_)
+    df.to_csv(csv_path + '/cv_results.csv', index=False)
+
+    # cv = StratifiedKFold(n_splits=5)
+    # scores = cross_val_score(pipe, df[x_col_list], df[y_col], cv=cv, scoring='roc_auc')
+    # print(scores.mean())
 
     # result = pipe.fit_transform(X=df[x_col_list], y=df[y_col])
 
